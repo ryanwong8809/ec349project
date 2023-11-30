00f677b75e31a153c2f6d43957f7b579575ba3a3
Ryan Wong <ryanwong8809@gmail.com>
2023-11-30 00:49:38 +0000

    30/11 5 days left!!!

--- ec349test.R
+++ ec349test.R
@@ -1,3 +1,15 @@
 print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
+
+install.packages("caret")
+library(caret)
+set.seed(1)
+# Assuming your dataset is stored in a data frame called 'your_data'
+# The 'p' parameter determines the proportion of the data to be used for training
+# The result is a list of indices for the training set
+train_indices <- createDataPartition(review_data_small$stars, p = 0.8, list = FALSE)
+# Create the training set
+training_set <- review_data_small[train_indices, ]
+# Create the test set
+test_set <- review_data_small[-train_indices, ]
