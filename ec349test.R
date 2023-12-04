2cf283d5b5c30b6d87491864d1914d14ceb7b467
Ryan Wong <ryanwong8809@gmail.com>
2023-12-04 02:34:41 +0000

    LESGO

--- ec349test.R
+++ ec349test.R
@@ -15,8 +15,7 @@ review_data_training_set <- review_data_small[train_indices, ]
 review_data_test_set <- review_data_small[-train_indices, ]
 
 #install tidy text and dplyr
-install.packages(c("tidytext", "dplyr"))
-install.packages("textdata")
+install.packages(c("tidytext", "dplyr", "textdata"))
 library(tidytext)
 library(dplyr)
 library("textdata")
@@ -24,12 +23,20 @@ library("textdata")
 # Load sentiment lexicon (AFINN as an example)
 afinn_scores <- get_sentiments("afinn")
 
-# Preprocess the text data
-review_data_small_tokens <- review_data_small %>%
+# Add a unique identifier to each row so that after tokenization, I can join the tokenized data back to the original data frame based on this identifier.
+review_data_training_set <- review_data_training_set %>%
+  mutate(review_id = row_number())
+
+# Tokenize the text data
+review_data_training_set_tokens <- review_data_training_set %>%
   unnest_tokens(word, text)
 
-# Join tokenized data with sentiment scores
-review_data_small_sentiment <- review_data_small_tokens %>%
-  left_join(afinn_scores) %>%
-  group_by(text) %>%
-  summarise(sentiment_score = sum(score, na.rm = TRUE))
+# Perform sentiment analysis and score each word using AFINN lexicon
+review_data_training_set_sentiment <- review_data_training_set_tokens %>%
+  inner_join(afinn_scores, by = "word") %>%
+  group_by(review_id) %>%
+  summarise(sentiment_score = sum(value))
+
+# Join the sentiment scores back to the original data frame based on the unique identifier (review_id)
+review_data_training_set <- review_data_training_set %>%
+  left_join(review_data_training_set_sentiment, by = "review_id")
